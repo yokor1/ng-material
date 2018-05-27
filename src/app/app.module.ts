b33832f814c23b0938ce25769d1573caeb4db401
Youcef Korichi <youcef.korichi.1@ulaval.ca>
2018-05-27 18:52:24 -0400

    refactor: header-component + sidenav-list-component

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -14,6 +14,8 @@ import {PastTrainingComponent} from './training/past-training/past-training.comp
 import {WelcomeComponent} from './welcome/welcome.component';
 import {AppRoutingModule} from './app-routing/app-routing.module';
 import {FormsModule} from '@angular/forms';
+import { HeaderComponent } from './navigation/header/header.component';
+import { SidenavListComponent } from './navigation/sidenav-list/sidenav-list.component';
 
 @NgModule({
   declarations: [
@@ -24,7 +26,9 @@ import {FormsModule} from '@angular/forms';
     CurrentTrainingComponent,
     NewTrainingComponent,
     PastTrainingComponent,
-    WelcomeComponent
+    WelcomeComponent,
+    HeaderComponent,
+    SidenavListComponent
   ],
   imports: [
     BrowserModule,

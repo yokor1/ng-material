32600016bd57a78bc5bc123f8c7a5fc911c71335
Youcef Korichi <youcef.korichi.1@ulaval.ca>
2018-06-03 02:30:39 -0400

    adding authService

---
+++ src/app/auth/auth.service.ts
@@ -0,0 +1,43 @@
+import { Injectable } from '@angular/core';
+import { User } from './user.model';
+import { AuthData } from './auth-data.model';
+import { Subject } from 'rxjs';
+
+@Injectable({
+  providedIn: 'root'
+})
+export class AuthService {
+  authChange = new Subject<boolean>();
+  private user: User;
+
+  constructor() { }
+
+  registerUser(authData: AuthData) {
+    this.user = {
+      email: authData.email,
+      userId: Math.round(Math.random() * 10000).toString()
+    };
+    this.authChange.next(true);
+  }
+
+  login(authData: AuthData) {
+    this.user = {
+      email: authData.email,
+      userId: Math.round(Math.random() * 10000).toString()
+    };
+    this.authChange.next(true);
+  }
+
+  logout() {
+    this.user = null;
+    this.authChange.next(false);
+  }
+
+  getUser() {
+    return Object.assign({}, this.user);
+  }
+
+  isAuth() {
+    return this.user != null;
+  }
+}
--- src/app/auth/login/login.component.ts
+++ src/app/auth/login/login.component.ts
@@ -1,4 +1,6 @@
 import {Component, OnInit} from '@angular/core';
+import { FormGroup, FormControl, Validators, NgForm } from '@angular/forms';
+import { AuthService } from '../auth.service';
 
 @Component({
   selector: 'app-login',
@@ -6,11 +8,27 @@ import {Component, OnInit} from '@angular/core';
   styleUrls: ['./login.component.css']
 })
 export class LoginComponent implements OnInit {
+  loginForm: FormGroup;
 
-  constructor() {
+  constructor(private authService: AuthService) {
   }
 
   ngOnInit() {
+    this.loginForm = new FormGroup({
+      email: new FormControl('', {
+        validators: [Validators.required, Validators.email]
+      }),
+      password: new FormControl('', {
+        validators: [Validators.required, Validators.minLength(6)]
+      })
+    });
+  }
+
+  onSubmit(form: NgForm) {
+    this.authService.login({
+      email: form.value.email,
+      password: form.value.password
+    });
   }
 
 }

00645a7f7b26edf32cb65bedbb66278ee28450ee
Youcef Korichi <youcef.korichi.1@ulaval.ca>
2018-06-09 22:57:45 -0400

    adding: firebase

--- src/app/training/new-training/new-training.component.ts
+++ src/app/training/new-training/new-training.component.ts
@@ -1,7 +1,11 @@
 import {Component, OnInit, EventEmitter, Output} from '@angular/core';
+import {AngularFirestore} from 'angularfire2/firestore';
+
 import { TrainingService } from '../training.service';
 import { Exercise } from '../exercise.model';
 import { NgForm } from '@angular/forms';
+import { Observable } from 'rxjs';
+import { map, flatMap } from 'rxjs/operators';
 
 @Component({
   selector: 'app-new-training',
@@ -9,12 +13,23 @@ import { NgForm } from '@angular/forms';
   styleUrls: ['./new-training.component.css']
 })
 export class NewTrainingComponent implements OnInit {
-  exercises: Exercise[];
+  exercises: Observable<any>;
 
-  constructor(private trainingService: TrainingService) {}
+  constructor(
+    private trainingService: TrainingService,
+    private db: AngularFirestore
+    ) {}
 
   ngOnInit() {
-    this.exercises = this.trainingService.getAvailableExercises();
+    this.exercises =  this.db.collection('availableExercises')
+    .snapshotChanges()
+    .pipe(
+      map(docs => docs
+        .map(doc => Object.assign({
+        id: doc.payload.doc.id
+      }, doc.payload.doc.data())))
+    );
+
   }
 
   onStartTraining(form: NgForm) {

dbf55bf7c1eb0b442f8ae28a5df4d4e9af35ca8d
Youcef Korichi <youcef.korichi.1@ulaval.ca>
2018-06-10 18:14:50 -0400

    prod tests

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,3 +1,11 @@
 export const environment = {
-  production: true
+  production: true,
+  firebase: {
+    apiKey: 'AIzaSyC9oCcbNki9cFPkMnwRQdkouSwYxHNev_A',
+    authDomain: 'ng-training-c53db.firebaseapp.com',
+    databaseURL: 'https://ng-training-c53db.firebaseio.com',
+    projectId: 'ng-training-c53db',
+    storageBucket: 'ng-training-c53db.appspot.com',
+    messagingSenderId: '252226910833'
+  }
 };
